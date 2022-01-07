2e984596d8f32d2da5f40f93b0038abacd5bf48a
Alan Shen <alanshen111@gmail.com>
2022-01-06 23:18:19 -0800

    readable

--- facts.js
+++ facts.js
@@ -8,7 +8,7 @@ Module.register("facts",{
 		facts: { 
 			art: [
 				"Purple is the color for epilepsy awareness because of its association with lavender, which is a traditional herbal treatment for the disease.",
-                "Vincent Van Gogh painted The Starry Night during his stay at a psychiatric facility.",
+				"Vincent Van Gogh painted The Starry Night during his stay at a psychiatric facility.",
 			],
 			food: [
 				"Do not eat polar bear liver. The human body can not handle that much vitamin A.",
@@ -16,13 +16,13 @@ Module.register("facts",{
 			],
 			health: [
 				"There are no muscles in your fingers: Their function is controlled by muscles in your palms and arms.",
-                "The flashes of colored light you see when you rub your eyes are called “phosphenes”.",
+				"The flashes of colored light you see when you rub your eyes are called “phosphenes”.",
 				"On average, a 150-pound person burns 114 calories per hour while standing and doing nothing.",
 			],
-            history: [
+			history: [
 				"Pepsi, the soft drink company, once became the 6th largest military in the world after the Soviets exchanged 17 submarines, one frigate, one cruiser, and one destroyer for three billion dollars' worth of Pepsi.",
-                "The shortest war, fought between Britain and Zanzibar, lasted 38 minutes and ended with a British victory.",
-                "Nobody knows who invented the fire hydrant because the patent for the hydrant was lost in a fire.",
+				"The shortest war, fought between Britain and Zanzibar, lasted 38 minutes and ended with a British victory.",
+				"Nobody knows who invented the fire hydrant because the patent for the hydrant was lost in a fire.",
 				"It took humanity 4 times longer to switch from bronze swords to steel swords than it took humanity to switch from steel swords to nuclear bombs.",
 				"Canadians said “sorry” so much that a law was passed in 2009 declaring that an apology could not be used as evidence of admission to guilt.",
 				"Cleopatra lived closer in time to the Moon landing than to the construction of the Great Pyramid of Giza.",
@@ -30,26 +30,26 @@ Module.register("facts",{
 			language: [
 				"A single strand of Spaghetti is called a “Spaghetto”.",
 				"pneumonoultramicroscopicsilicovolcanoconiosis, a lung disease caused by inhaling silicate, is the longest word in english.",
-                "To leave a party without telling anyone is called in English, a “French Exit”. In French, it is called a “partir à l’anglaise”, to leave like the English.",
+				"To leave a party without telling anyone is called in English, a “French Exit”. In French, it is called a “partir à l’anglaise”, to leave like the English.",
 			],
-            nature: [
+			nature: [
 				"Armadillos are bulletproof.",
-                "At over 2,000 kilometers long, The Great Barrier Reef is the largest living structure on Earth.",
-                "Cows don’t actually have four stomachs; they have one stomach with four compartments.",
-                "It's illegal to own just one guinea pig in Switzerland because they get lonely.",
+				"At over 2,000 kilometers long, The Great Barrier Reef is the largest living structure on Earth.",
+				"Cows don’t actually have four stomachs; they have one stomach with four compartments.",
+				"It's illegal to own just one guinea pig in Switzerland because they get lonely.",
 				"If it is raining and the birds are still loud outside, the rain is going to continue for a long time (8-10+ hours). They're basically panicking and trying to solve the problem of running out of food soon.",
 			],
-            nerd: [
-                "A million seconds is 12 days. A billion seconds is 31 years. A trillion seconds is 31,688 years.",
+			nerd: [
+				"A million seconds is 12 days. A billion seconds is 31 years. A trillion seconds is 31,688 years.",
 				"Every 60 seconds in Africa, a minute passes.",
 			],
-            space: [
-                "There are more trees on Earth than stars in the Milky Way.",
-                "Uranus used to be named George."
-            ],
+			space: [
+				"There are more trees on Earth than stars in the Milky Way.",
+				"Uranus used to be named George."
+			],
 			tips: [
 				"The more you say, the more you are responsible for.",
-                "When you don't have all the facts, try to give people the most generous reason you can for their behaviour. Annoyingly slow driver? Maybe it's a mom with a birthday cake in the back. This mindset will gradually make you less reactive, more compassionate and more forgiving of your own bad days.",
+				"When you don't have all the facts, try to give people the most generous reason you can for their behaviour. Annoyingly slow driver? Maybe it's a mom with a birthday cake in the back. This mindset will gradually make you less reactive, more compassionate and more forgiving of your own bad days.",
 				"Do not try to be the man your father would want you to be. Be the man you would like your son to be be.",
 				"Avoid being the bearer of bad news.",
 				"Inale twice, exhale once. This will help you intake some more oxygen.",
